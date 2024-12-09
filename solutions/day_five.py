76379102573c3e89e10138bfb44084d92b8308e1
Jacob Veatch <jakedveatch@gmail.com>
2024-12-09 10:08:37 -0600

    made create day script

---
+++ solutions/day_five.py
@@ -0,0 +1,5 @@
+from input import read_input_file, InputFile
+
+def day_five_part_1():
+    inp: InputFile = read_input_file("inputs/day_five.txt")
+    ordering_rules =
---
+++ solutions/day_six.py
@@ -0,0 +1,8 @@
+from input import read_input_file, InputFile
+
+def day_six_part_1():
+    inp: InputFile = read_input_file("inputs/day_day_six.txt")
+
+
+def day_six_part_2():
+    inp: InputFile = read_input_file("inputs/day_day_six.txt")
